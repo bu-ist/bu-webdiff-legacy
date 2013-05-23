e251420fa496148bff476d040c014ae15cd9494a
Niall Kavanagh <ntk@bu.edu>
2013-05-23 18:15:05 -0400

    Adding in scripts to take screenshots, and then compare those screenshots

---
+++ compare.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+urldecode(){
+  echo -e "$(sed 's/+/ /g;s/%\(..\)/\\x/g;')"
+}
+
+FIRST="$1"
+SECOND="$2"
+
+if [ $# -ne 2 ]
+then
+	printf "Usage: `basename %s` {first directory} {second directory}\n" $0
+	printf "Compares the screenshots in the first directory to those in "
+	printf "the second.\n"
+ 	exit 1
+fi
+
+if [ ! -d "$FIRST" ]; then
+	printf "'%s' does not exist\n" $FIRST
+	exit 1
+fi
+
+if [ ! -d "$SECOND" ]; then
+	printf "'%s' does not exist\n" $SECOND
+	exit 1
+fi
+
+for filename in $FIRST/*.png ; do
+	filename=`sed "s|^$FIRST/||" <<< $filename`
+	compare "$FIRST/$filename" "$SECOND/$filename" -metric AE difference.png > /dev/null 2>&1
+	
+	if [ "$?" -ne "0" ]; then
+		url=`sed "s/.png$//" <<< $filename`
+		url=`sed "s/%\([0-9A-F][0-9A-F]\)/\\\\\x\1/g" <<< $url`
+		echo -e "$url does not match"
+	fi
+done
+
+rm difference.png
---
+++ grab.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+import argparse
+import os
+import urllib
+
+def url_to_filename(url):
+	filename = url.replace('http://', '')
+	filename = filename.replace('/', '#')
+	return filename
+
+def main():
+	# command line args
+	parser = argparse.ArgumentParser(description='Load a series of URLs and \
+	take a screenshot.')
+	
+	parser.add_argument('runname', help='the name for this run')
+	
+	parser.add_argument('--urls', default='urls.txt', help='the filename \
+	containing the list of URLs to load')
+	
+	args = parser.parse_args()
+	
+	if not os.path.exists(args.runname):
+	    os.makedirs(args.runname)
+	
+	# setup our browser
+	browser = webdriver.Firefox()
+	browser.set_window_size(1200, 800)
+	
+	# load urls
+	urls = open(args.urls).read().splitlines()
+
+	# grab screenshots
+	for url in urls:
+		browser.get(url)
+		filename = "%s/%s.png" % (args.runname, urllib.quote_plus(url))
+		print 'Grabbing %s to %s' % (url, filename)
+		browser.save_screenshot(filename)
+
+	browser.quit()
+
+if __name__ == "__main__":
+		main()
