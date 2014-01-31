d555abc99a363829aea945f48db302db8bf42336
Niall Kavanagh <ntk@bu.edu>
2014-01-31 12:24:21 -0500

    Improved directory handling
    grab.py expects a path argument, used to store images.
    compare.sh can handle absolute and relative paths to compare.

--- compare.sh
+++ compare.sh
@@ -1,47 +1,48 @@
 #!/bin/bash
 
 urldecode(){
-  echo -e "$(sed 's/+/ /g;s/%\(..\)/\\x/g;')"
+    echo -e "$(sed 's/+/ /g;s/%\(..\)/\\x/g;')"
 }
 
-FIRST="$1"
-SECOND="$2"
+first=$(cd -P -- "$1" && printf "%s\n" "$(pwd -P)")
+second=$(cd -P -- "$2" && printf "%s\n" "$(pwd -P)")
 
 if [ $# -ne 2 ]
 then
-	printf "Usage: `basename %s` {first directory} {second directory}\n" $0
-	printf "Compares the screenshots in the first directory to those in "
-	printf "the second.\n"
- 	exit 1
+    printf "Usage: `basename %s` {first directory} {second directory}\n" $0
+    printf "Compares the screenshots in the first directory to those in "
+    printf "the second.\n"
+    exit 1
 fi
 
-if [ ! -d "$FIRST" ]; then
-	printf "'%s' does not exist\n" $FIRST
-	exit 1
+if [ ! -d "$first" ]; then
+    printf "'%s' does not exist\n" $first
+    exit 1
 fi
 
-if [ ! -d "$SECOND" ]; then
-	printf "'%s' does not exist\n" $SECOND
-	exit 1
+if [ ! -d "$second" ]; then
+    printf "'%s' does not exist\n" $second
+    exit 1
 fi
 
-DIFFERENCES="$FIRST-vs-$SECOND"
+differences=$(printf "%s/%s-vs-%s" "$(dirname -- "$first")" \
+    "$(basename -- "$first")" "$(basename -- "$second")")
 
-if [ ! -d "$DIFFERENCES" ]; then
-	printf "Creating '%s' to store difference images\n" $DIFFERENCES
-	mkdir $DIFFERENCES
+if [ ! -d "$differences" ]; then
+    printf "Creating '%s' to store difference images\n" $differences
+    mkdir $differences
 fi
 
-for filename in $FIRST/*.png ; do
-	filename=`sed "s|^$FIRST/||" <<< $filename`
-	compare "$FIRST/$filename" "$SECOND/$filename" -metric AE "$DIFFERENCES/$filename" > /dev/null 2>&1
-	
-	if [ $? -eq 0 ]; then
-		# printf "Removing %s\n" "$DIFFERENCES/$filename"
-		rm "$DIFFERENCES/$filename"
-	else
-		url=`sed "s/.png$//" <<< $filename`
-		url=`sed "s/%\([0-9A-F][0-9A-F]\)/\\\\\x\1/g" <<< $url`
-		echo -e "$url does not match"
-	fi
+for filename in $first/*.png ; do
+    filename=`sed "s|^$first/||" <<< $filename`
+    compare "$first/$filename" "$second/$filename" -metric AE "$differences/$filename" > /dev/null 2>&1
+
+    if [ $? -eq 0 ]; then
+        # printf "Removing %s\n" "$differences/$filename"
+        rm "$differences/$filename"
+    else
+        url=`sed "s/.png$//" <<< $filename`
+        url=`sed "s/%\([0-9A-F][0-9A-F]\)/\\\\\x\1/g" <<< $url`
+        echo -e "$url does not match"
+    fi
 done
--- grab.py
+++ grab.py
@@ -1,53 +1,64 @@
 #!/usr/bin/env python
+from __future__ import print_function
 from selenium import webdriver
 import argparse
 import os
 import urllib
+import sys
+
 
 def url_to_filename(url):
-	filename = url.replace('http://', '')
-	filename = filename.replace('/', '%2F')
-	return filename
+    filename = url.replace('http://', '')
+    filename = filename.replace('/', '%2F')
+    return filename
+
 
 def main():
-	# command line args
-	parser = argparse.ArgumentParser(description='Load a series of URLs and \
-	take a screenshot.')
-	
-	parser.add_argument('runname', help='the name for this run')
-	
-	parser.add_argument('--urls', default='urls.txt', help='the filename \
-	containing the list of URLs to load')
-	
-	args = parser.parse_args()
-	
-	if not os.path.exists(args.runname):
-	    os.makedirs(args.runname)
-	
-	# setup our browser
-	profile = webdriver.FirefoxProfile()
+    # command line args
+    parser = argparse.ArgumentParser(description='Load a series of URLs and \
+                                     take a screenshot.')
 
-	profile.set_preference("browser.download.folderList", 2)
-	# profile.set_preference("javascript.enabled", False)
+    parser.add_argument('directory', help='directory to save images to')
 
-	browser = webdriver.Firefox(firefox_profile=profile)
-	browser.set_window_size(1200, 800)
-	
-	# load urls
-	urls = open(args.urls).read().splitlines()
+    parser.add_argument('--urls', default='urls.txt', help='the filename \
+                        containing the list of URLs to load')
 
-	# grab screenshots
-	for url in urls:
-		browser.get(url)
-		filename = "%s/%s.png" % (args.runname, urllib.quote_plus(url))
-		print '[-] %s' % (url)
-		
-		try:
-			browser.save_screenshot(filename)
-		except:
-			print '[!] ERROR %s' % (url)
+    args = parser.parse_args()
 
-	browser.quit()
+    if not os.path.exists(args.directory):
+        try:
+            os.mkdir(args.directory)
+        except OSError as e:
+            print('* Could not create directory "{0}": \
+                  {1} ({2})'.format(args.directory, e.message, e.errno),
+                  file=sys.stderr)
+            sys.exit(1)
+
+    # setup our browser
+    profile = webdriver.FirefoxProfile()
+
+    profile.set_preference("browser.download.folderList", 2)
+    # profile.set_preference("javascript.enabled", False)
+
+    browser = webdriver.Firefox(firefox_profile=profile)
+    browser.set_window_size(1200, 800)
+
+    # load urls
+    urls = open(args.urls).read().splitlines()
+
+    # grab screenshots
+    for url in urls:
+        browser.get(url)
+        filename = os.path.join(args.directory,
+                                urllib.quote_plus(url) + '.png')
+        print(url)
+
+        try:
+            browser.save_screenshot(filename)
+        except:
+            print('* ERROR {0}'.format(url))
+
+    browser.quit()
 
 if __name__ == "__main__":
-		main()
+    main()
